d24f796b4bb59ada3ae3b66fda451c0d98cf7173
Eliyas Hossain <eliyashossain700@gmail.com>
2022-03-16 00:32:38 +0600

    Option-3 Completed

--- js/main.js
+++ js/main.js
@@ -5,23 +5,38 @@ const loadData=()=>{
 }
 const display=document.getElementById("countries")
 const displayData= countries=>{
+    console.log(countries[0])
     const countryHtml=countries.map(country=> getCountryHtml(country))
     display.innerHTML=countryHtml.join(' ');
 
 }
-const getCountryHtml=country=>{
-    console.log(country.flags.png)
-    // Option-2 Destructuring
-    const {name, flag}=country;
-    return `
-    <div class="country">
-        <h5>Name:${name}</h5>
-        <img width="240px" src="${flag}"></div>`
-}
 /* Option-1  */
+// const getCountryHtml=country=>{
 //     return `
 //     <div class="country">
 //         <h5>Name:${country.name}</h5>
 //         <img width="240px" src="${country.flag}"></div>`
 // }
+// Option-2 Destructuring
+// const getCountryHtml=country=>{
+//     console.log(country.flags.png)
+    
+//     const {name, flag}=country;
+//     return `
+//     <div class="country">
+//         <h5>Name:${name}</h5>
+//         <img width="240px" src="${flag}"></div>`
+// }
+
+/* Option-3 Destructuring Inside the Function Parameter */
+const getCountryHtml=({name, flag,capital,area,region})=>{
+    return `
+    <div class="country">
+        <h5>Name:${name}</h5>
+        <p>Capital:${capital}</p>
+        <p>Area:${area}</p>
+        <small>Continent:${region}</small>
+        <img width="240px" src="${flag}"></div>`
+}
+
 loadData()
